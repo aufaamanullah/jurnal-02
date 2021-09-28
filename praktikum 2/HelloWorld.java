f997056152b3ab16469d05520771057f4ac28e63
aufaamanullah <aufa27071997@gmail.com>
2021-09-28 09:11:17 +0700

    HelloWorld.java

---
+++ praktikum 2/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama:muhammad aufa amanullah");
+		System.out.println("NIM:6706213160");
+		System.out.println("KELAS:45-03");
+   }
+}
